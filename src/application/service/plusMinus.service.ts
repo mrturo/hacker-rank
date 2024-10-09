c5ceed27c1d27f0e70af8222a83c4dce46fdd01d
Arturo Mendoza <amendoza@alemana.cl>
2024-10-09 15:40:59 -0300

    feat(plusMinus): Plus Minus Algorithm

--- src/application/service/plusMinus.service.ts
+++ src/application/service/plusMinus.service.ts
@@ -1,14 +1,9 @@
 import { PlusMinus as PlusMinusModel } from '../../domain/model/plusMinus.model';
 export class PlusMinus {
-  public static exe(arr: number[] | undefined = undefined): PlusMinusModel {
-    let positives: number | undefined = undefined;
-    let negatives: number | undefined = undefined;
-    let zeros: number | undefined = undefined;
-    if (arr) {
-      positives = arr.filter((a: number) => a > 0).length;
-      negatives = arr.filter((a: number) => a < 0).length;
-      zeros = arr.filter((a: number) => a === 0).length;
-    }
+  public static exe(arr: number[] = []): PlusMinusModel {
+    const positives = arr.filter((a: number) => a > 0).length;
+    const negatives = arr.filter((a: number) => a < 0).length;
+    const zeros = arr.filter((a: number) => a === 0).length;
     return new PlusMinusModel(positives, negatives, zeros);
   }
 }
